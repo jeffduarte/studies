b3bfdf251e568618878868906a9c559b4dcb2f70
Jeferson Change <Jefersonchange@gmail.com>
2018-04-03 18:07:22 -0300

    php p q entende aula 11 sqlupdate

--- PHP-train/php-devclass/app/functions/database.php
+++ PHP-train/php-devclass/app/functions/database.php
@@ -45,8 +45,33 @@ function all($table){
 
 }
 
-function update() {
+function update($table, $fields, $where) {
+    if (!is_array($fields)){
+        $fields = (array) $fields;
+    }
 
+    $pdo = connect();
+
+    $data = array_map(function ($field){
+        return "$field = :$field";
+    }, array_keys($fields));
+
+    
+
+    $sql = "update $table set ";
+
+    $sql .= implode(',',$data);
+
+    $sql .= " where {$where[0]} = :{$where[0]}";
+
+    // dd($sql);
+
+    $data = array_merge($fields, [$where[0] => $where[1]]);
+
+    $update = $pdo->prepare($sql);
+    $update->execute($data);
+
+    return $update->rowCount();
 }
 
 function find($table, $field, $value) {
