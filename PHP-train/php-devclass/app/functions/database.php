40171858657db1e0d538716336be83d4eccbe480
Jeferson Change <Jefersonchange@gmail.com>
2018-04-03 16:36:44 -0300

    php p quem entende aula 10 edit user

---
+++ PHP-train/php-devclass/app/functions/database.php
@@ -0,0 +1,68 @@
+<?php
+
+function connect(){
+    $pdo = new \PDO("mysql:host=localhost;dbname=system_train;charset=utf8", 'root', 'password');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+    return $pdo;
+
+}
+
+
+
+function create($table, $fields) {
+    // $pdo = connect();
+    
+    if(!is_array($fields)){
+        $fields = (array) $fields;
+    }
+    
+    $sql = "insert into $table";
+    
+    $sql .= "(" . implode(',', array_keys($fields)) . ")";
+    
+    $sql .= " values(" . ":" . implode(',:', array_keys($fields)) . ")";
+
+    $pdo = connect();
+
+    $insert = $pdo->prepare($sql);
+
+    return $insert->execute($fields);
+
+
+}
+
+function all($table){
+    $pdo = connect();
+
+    $sql = "select * from $table";
+    $list = $pdo->query($sql);
+
+    $list->execute();
+
+    return $list->fetchAll();
+
+}
+
+function update() {
+
+}
+
+function find($table, $field, $value) {
+    $pdo = connect();
+
+    $value = filter_var($value, FILTER_SANITIZE_NUMBER_INT);
+
+    $sql = "select * from $table where $field = :$field";
+
+    $find = $pdo->prepare($sql);
+    $find->bindValue($field, $value);
+    $find->execute();
+
+    return $find->fetch();
+}
+
+function delete() {
+
+}
